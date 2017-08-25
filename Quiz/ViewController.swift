add460c419f2750d3b3bed1534bfb0ca59b6557d
Benjamin Dagg <dagg9715@M506-246-25987.local>
2017-08-24 18:31:06 -0700

    first commit

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    //link the views in main storyboard to memory locations
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    //array of questions
+    let questions: [String] = [
+        "What is 7 + 7?",
+        "What is the capital of Vermont?",
+        "What is a cognac made from?"
+    ]
+    
+    //array of answers
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    //tracks current question
+    var currentQuestionIndex: Int = 0
+    
+    //overloads viewDidLoad to change question label on startup to first question
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //this function is called when question label is pressed
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        
+        currentQuestionIndex = (currentQuestionIndex + 1) % 3
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+    }
+    
+    //this function is called when show answer button is presssed
+    @IBAction func showAnswer(_ sender: UIButton){
+        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+        
     }
 
 
